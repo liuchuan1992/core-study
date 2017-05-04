13777ccafe4ff6d56c995f06fd126fccf12966a3
Lv.CanFeng <lv.canfeng@geely.com>
2017-05-04 11:08:06 +0800

    TestStream commit

---
+++ study/src/main/java/core/java8/StreamTest.java
@@ -0,0 +1,20 @@
+package core.java8;
+
+import java.util.Arrays;
+
+/**
+ * @author : lvcf on 2017/5/4 11:07
+ * @Description :
+ */
+public class StreamTest {
+
+    public static void main(String[] args) {
+        String[] a = {"1.0", "2.0", "3.0", "4.0", "5.0"};
+        Arrays.stream(a)
+                .map((v) -> Double.valueOf(v)) //String转Double
+                .filter((v) -> v > 2) //过滤
+                .sorted((v1, v2) -> v2.compareTo(v1))//降序排列
+                .limit(2)
+                .forEach(v -> System.out.println(v));//终止操作 循环输出
+    }
+}
